06c1425ff154291d70d1080d855353c330e27100
LuuHoangBac <bacforstudy@gmail.com>
2018-04-02 23:32:54 +0700

    Test04-day3

--- Test04__form/js/test04.js
+++ Test04__form/js/test04.js
@@ -9,19 +9,54 @@
 // });
 
 $(function () {
-  $('.page1').hide();
-  $('#type').change(function () {
-    if ($('#type').val() == 'opt1') {
-      $('.page1').show();
-      $('.page2').hide();
-    }
-    if ($('#type').val() == 'opt2') {
-      $('.page2').show();
-      $('.page1').hide();
-    }
-    else{
-      $('.page2').hide();
-      $('.page1').hide();
-    }
+  // var width = $(window).width();
+  // if (width <= 768) {
+    $('.page0').show();
+    $('.page1').hide();
+    $('.page2').hide();
+    $('.page12').hide();
+    $('.page12end').hide();
+    $('.page34end').hide();
+
+    $('#type').change(function () {
+      if ($('#type').val() == 'opt1') {
+        $('.page0').show();
+        $('.page1').hide();
+        $('.page2').hide();
+        $('.page12').hide();
+        $('.page12end').hide();
+        $('.page34end').hide();
+      }
+      else if ($('#type').val() == 'opt2') {
+        $('.page0').hide();
+        $('.page1').show();
+        $('.page2').hide();
+        $('.page12').show();
+        $('.page12end').show();
+        $('.page34end').hide();
+      }
+      else if ($('#type').val() == 'opt3') {
+        $('.page0').hide();
+        $('.page1').hide();
+        $('.page2').show();
+        $('.page12').show();
+        $('.page12end').show();
+        $('.page34end').hide();
+      }
+      else {
+        $('.page0').hide();
+        $('.page1').hide();
+        $('.page2').hide();
+        $('.page12').hide();
+        $('.page12end').hide();
+        $('.page34end').hide();
+      }
+    });
+});
+
+$(document).ready(function(){
+  $("#addrow").click(function(){
+      // $("#tdaddrow").append('HAHA');
+      $('#tdaddrow').prepend($('<input type="date" name="" id=""  class="formcontainer__inputdate" placeholder="例） 0000/00/00" required/>'));
   });
 });
