b9732e6f3230d9caf53df797ebfd36080a3135c8
QNam <m.q.nam@alive-web.co.jp>
2018-04-06 17:15:05 +0700

    panet

---
+++ panethome/libs/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+
+  <!-- <link rel="stylesheet" href="../css/panethome.css">
+  <link rel="stylesheet" href="../css/common.css"> -->
+
+</head>
+<body>
+<header class="head">
+    <div class="clearfix headcontainer">
+      <div class="head__logo">
+        <a href="a">
+          <img src="images/logohead.jpg" alt="Logo" class="resize1">
+        </a>
+      </div>
+      <div class="head__phone">
+        <a href="a">
+          <img src="images/phoneNum.png" alt="Phone">
+        </a>
+        <button class="btn1 btn1--black" type="button">見学会・イベント予約</button>
+        <button class="btn1 btn1--red" type="button">お問い合わせ・資料請求</button>
+      </div>
+      <div class="head__buttonsNav clearfix">
+        <button class="btnsp btnsp--red" type="button">お問い合わせ資料請求</button>
+        <button class="btnsp btnsp--black" type="button">メニュー</button>
+      </div>
+    </div>
+    <div class="headcontainer menu">
+      <a href="" class="menu__uptext">TOP</a>
+      <a href="a" class="menu__uptext">パネットホームについて</a>
+      <a href="a" class="menu__uptext">施工実績</a>
+      <a href="a" class="menu__uptext">見学会・イベント情報</a>
+      <a href="a" class="menu__uptext">会社概要・代表挨拶</a>
+      <a href="a" class="menu__uptext">お客様インタビュー</a>
+      <a href="a" class="menu__uptext">家づくりコラム</a>
+    </div>
+
+    <!--<ul class="clearfix hideSP">
+        <li class="arr">
+          <p>
+              TOP <img src="images/arr_right.png" alt="Arrow" class="maxwid10"> </p>
+          <p>
+              パネットホームについて <img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          <p>
+              理想の住まいを叶えるパッシブデザイン<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          <p>
+              快適・高気密・高断熱・高耐震の住まい<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+        </li>
+
+        <li class="arr clearfix">
+          <div class="twocol twocol-mar twocol-martop">
+            <p>
+                施工実績 <img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+
+          <div class="twocol twocol-mar twocol-martop">
+            <p>
+                お客様インタビュー<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+          <div class="twocol twocol-mar">
+            <p>
+                会社概要代表挨拶<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+          <div class="twocol twocol-mar">
+            <p>
+                採用情報<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+          <div class="twocol twocol-mar">
+            <p>
+                見学会イベント情報<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+          <div class="twocol twocol-mar">
+            <p>
+                家づくりコラム<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+          <div class="twocol twocol-mar">
+            <p>
+                プライバシーポリシー<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+          <div class="twocol twocol-mar">
+            <p>
+                サイトマップ<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+          </div>
+
+        </li>
+        <li class="footArr1">
+          <p>
+              お問い合わせ・資料請求<img src="images/arr_right.png" alt="Arrow" class="maxwid10"></p>
+        </li>
+      </ul>-->
+  </header>
+</body>
+</html>
