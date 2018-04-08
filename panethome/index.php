8e78897340f1d6ac93ec798e845a76f0ba48f00e
LuuHoangBac <bacforstudy@gmail.com>
2018-04-08 21:38:21 +0700

    Sunday

--- panethome/index.php
+++ panethome/index.php
@@ -8,10 +8,191 @@
 
   <link rel="stylesheet" href="css/panethome.css">
   <link rel="stylesheet" href="css/common.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:bold">
 
 </head>
 <body>
-<?php include 'libs/header.php';?>
+<?php include 'libs/header.php'?>
+
+<section class="top01">
+  <img src="images/img_top01.jpg" alt="top01" class="widhun hidesp">
+  <img src="images/img_top01sp.jpg" alt="top01" class="widhun hide">
+  <h1 class="top01__title">家づくりコラム</h1>
+</section>
+
+<section class="container top02 clearfix">
+  <div>
+    <a href="a" class="top02__link1">PANET HOME</a> 
+    <p class="top02__gt">&gt;</p>
+    <a href="a" class="top02__link2">家づくりコラム</a>
+  </div>
+
+  <ul class="top02__col1">
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0201.jpg" alt="pic_top0201" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">美しい住まいで、新年を迎える</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0202.jpg" alt="pic_top0202" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">すぐれたデザインを生み出す「住まいづくりの作法」(外観フォルム編)トテキストテキストテキストテキストテキストテキストテキスト...</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0201.jpg" alt="sec 601" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">好きな生活をデザインする収納計画</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0203.jpg" alt="sec 601" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">住宅の保証保証は品質・性能のモノサシ</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0201.jpg" alt="pic_top0201" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">美しい住まいで、新年を迎える</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0202.jpg" alt="pic_top0202" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">すぐれたデザインを生み出す「住まいづくりの作法」(外観フォルム編)トテキストテキストテキストテキストテキストテキストテキスト...</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0201.jpg" alt="sec 601" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">好きな生活をデザインする収納計画</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0203.jpg" alt="sec 601" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">住宅の保証保証は品質・性能のモノサシ</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+    
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0201.jpg" alt="pic_top0201" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">美しい住まいで、新年を迎える</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <li class="top02__col1--bor">
+      <div class="top02__col1__1">
+        <img src="images/pic_top0202.jpg" alt="pic_top0202" class="widhun">
+      </div>
+      <div class="top02__col1__2">
+        <p class="top02__col1__date">2017/01/00</p>
+        <h2 class="top02__col1__title">すぐれたデザインを生み出す「住まいづくりの作法」(外観フォルム編)トテキストテキストテキストテキストテキストテキストテキスト...</h2>
+        <button type="button" class="btnsm btnsm--2">
+          <span class="hidesp">詳しく見る</span>
+        </button>
+      </div>
+    </li>
+
+    <div class="right">
+      <div class="pagination">
+        <div class="clearfix">
+          <button><img src="images/ico_page01.gif" alt=""></button>
+          <a href="#">1</a>
+          <a href="#">...</a>
+          <a href="#">5</a>
+          <a href="#" class="active">6</a>
+          <a href="#">7</a>
+          <a href="#">...</a>
+          <a href="#">16</a>
+          <button><img src="images/ico_page02.gif" alt=""></button>
+        </div>
+        <p class="paging"> 6/16 pages</p>
+      </div>
+      
+    </div>
+
+  </ul>
+
   
+  <div id="showmenu1">記事カテゴリーを選択</div>
+
+  <div class="top02__col2" id="menu1">
+    <p class="top02__col2__title">カテゴリー</p>
+    <p class="top02__col2__li">すべての記事（00）</p>
+    <p class="top02__col2__li">カテゴリー1（00）</p>
+    <p class="top02__col2__li">カテゴリー2（00）</p>
+    <p class="top02__col2__li">カテゴリー3（00）</p>
+  </div>
+</section>
+<?php include 'libs/footer.php'?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+<script src="js/panethome.js"></script>
 </body>
 </html>
