e5e4de703787ffdbbdcd8ca5091d710646651d14
LuuHoangBac <bacforstudy@gmail.com>
2018-04-16 00:07:44 +0700

    Sunday

---
+++ php/HTMLCV/index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Luu Hoang Bac CV</title>
+
+    <link rel="stylesheet" href="css/luuhoangbac.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:200,300,300i,400,700">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:100i,300,400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <section class="top01">
+        <div class="top01__title">
+            <h1 class="top01__name">Hello, I'm Bac</h1>
+            <span class="txt-rotate" data-period="2000" data-rotate='[ "i&apos;m a front-end developer", "i love to create things" ]'></span>
+            <div class="top01--wrap">
+                <a href="" class="top01__more" onclick="ascrollto('about'); return false;">More about me</a>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="top02">
+        <ul class="container" id="about">
+            <li class="top02__col1 zoom">
+                <img src="images/pic_02.jpg" alt="Avatar" class="widhun">
+            </li>
+            <li class="top02__col2">
+
+                <h2 class="top02__col2__about">About me</h2>
+
+                <p class="top02__col2__content">My name is Luu Hoang Bac. I’m a fourth-year student from Ho Chi Minh City University of Technology and Education.
+                    I major in Information Technology, specialized in Software Technology. In reply to your information about
+                    KMS Scholarship on my faculty's website page. I am pleased to apply for the Scholarship, for further
+                    information I enclosed my C.V with this. I also hope that I will have a chance to work with your company
+                    in the future.
+
+                    <br>
+                    <br>From my C.V, you will see that my GPA is 7.90/10. I meet the requirement on reading and writing skills,
+                    it's good enough to read documents and to write things related to the job. I also have teamwork skills
+                    and the ability to work in some areas such as C, C++, C#, CSS, HTML5, SQL and background in Android.
+
+
+                    <br>
+                    <br>In addition, I am active and enthusiastic. I am willing to learn and do what takes to get a job done
+                    well even under pressure or on far mission. Thank you so much for reading this.
+                </p>
+
+                <a href="https://www.facebook.com/XTBac" class="square" target="_blank">
+                    <i class="fa fa-facebook"></i>
+                </a>
+
+                <a href="https://github.com/Lincox" class="square" target="_blank">
+                    <i class="fa fa-github"></i>
+                </a>
+
+                <a href="https://www.instagram.com/xtbac/" class="square" target="_blank">
+                    <i class="fa fa-instagram"></i>
+                </a>
+
+                <div class="top02__btn">
+                    <a href="LuuHoangBac_CV.pdf" download class="btn btn--white">Download CV</a>
+                </div>
+            </li>
+        </ul>
+    </section>
+
+
+    <section class="top03">
+        <h2 class="title">Key skills</h2>
+        <ul class="container container--1">
+            <li class="fourcol">
+                <div class="circle">
+                    <i class="fa fa-cogs"></i>
+                </div>
+                <h3 class="fourcol__title">
+                    Back-end
+                </h3>
+                <p class="fourcol__content">I have knowledge in the area of C#, .NET, Entity Framework, C++ and CMS Wordpress</p>
+            </li>
+            <li class="fourcol">
+                <div class="circle">
+                    <i class="fa fa-desktop"></i>
+                </div>
+                <h3 class="fourcol__title">
+                    Front-end
+                </h3>
+                <p class="fourcol__content">I have knowledge in the area of HTML/CSS (writing by sass, BEM), Javascript, Bootstrap, jQuery</p>
+            </li>
+            <li class="fourcol">
+                <div class="circle">
+                    <i class="fa fa-code"></i>
+                </div>
+                <h3 class="fourcol__title">
+                    Database
+                </h3>
+                <p class="fourcol__content">I have knowledge in the area of Microsoft SQL Server, MySQL, SQLite, MongoDB and so on</p>
+            </li>
+            <li class="fourcol">
+                <div class="circle">
+                    <i class="fa fa-comments-o"></i>
+                </div>
+                <h3 class="fourcol__title">
+                    English
+                </h3>
+                <p class="fourcol__content">I meet the requirement of speaking, listening, writing and reading. That helps me working well</p>
+            </li>
+        </ul>
+
+    </section>
+
+
+
+    <section class="top04">
+        <h2 class="title">My road</h2>
+        <ul class="container container--1 clearfix">
+            <li class="twocol twocol--left">
+                <h3>
+                    <a href="https://alive-web.vn/" class="twocol__name" target="_blank">Internship at ALIVE company</a>
+                </h3>
+                <p class="twocol__date">March 2018 - Present</p>
+                <p class="twocol__team">
+                    <span>Position: </span>Front-end Developer &#124;
+                    <span>Languages: </span>HTML/CSS, JS, CMS Wordpress</p>
+                <p class="twocol__des">I did the projet with my team in second year when taked Windows Programming class. From this project, I studied
+                    C&#10723, LINQ languages and knew how to create a winform app. This project was done for managing an
+                    mobile phone store.</p>
+            </li>
+            <li class="twocol twocol--right">
+                <h3 class="twocol__name">Webite sharing content</h3>
+                <p class="twocol__date">October 2017 - May 2018</p>
+                <p class="twocol__team">
+                    <span>Team size: </span>2 &#124;
+                    <span>Languages: </span>.NET, SQL, HTML/CSS, JS</p>
+                <p class="twocol__des">This is a big project that I was done in the last 2 semesters for the final year at the university. This
+                    website give people a solution for 2 types of people, one who wants to provide contents to get money
+                    and one who pays money for those contents.</p>
+            </li>
+            <li class="twocol twocol--left">
+                <h3 class="twocol__name">Website debating for students and lecturers</h3>
+                <p class="twocol__date">September 2016 - January 2017</p>
+                <p class="twocol__team">
+                    <span>Team size: </span>4 &#124;
+                    <span>Languages: </span>Java, SQL, HTML, CSS, JS</p>
+                <p class="twocol__des">I did the projet with my team in third year when taked Web Programming class. From this project, I studied
+                    Java, SQL and HTML&#47;CSS to create a basic website. This project was done for reporting and debating
+                    online
+                </p>
+            </li>
+            <li class="twocol twocol--right">
+                <h3 class="twocol__name">Managing Mobile Phone Store</h3>
+                <p class="twocol__date">January 2016 - May 2016</p>
+                <p class="twocol__team">
+                    <span>Team size: </span>3 &#124;
+                    <span>Languages: </span>C#, SQL</p>
+                <p class="twocol__des">I did the projet with my team in second year when taked Windows Programming class. From this project, I studied
+                    C&#10723, LINQ languages and knew how to create a winform app. This project was done for managing an
+                    mobile phone store.</p>
+            </li>
+        </ul>
+
+    </section>
+
+    <section class="top06">
+        <h2 class="title title--white">Get in touch</h2>
+        <form method="post" action="contactform.php">
+            <ul class="container container--1 clearfix">
+                <li class="twocol top06__mar">
+                    <input type="text" name="name" id="" placeholder="Name &#42;" class="top06__inp top06__inp--left">
+                </li>
+                <li class="twocol top06__mar">
+                    <input type="text" name="mail" id="" placeholder="Email &#42;" class="top06__inp top06__inp--right">
+                </li>
+                <li class=" top06__mar">
+                    <input type="text" name="subject" id="" placeholder="Subject" class="top06__inp">
+                </li>
+                <li class=" top06__mar">
+                    <textarea name="message" id="comment" cols="30" rows="8" class="top06__inp" placeholder="Please write your message here &#42;"></textarea>
+                </li>
+                <li class="top06__btn">
+                    <button type="submit" name="submit" class="btn btn--key">Send email</button>
+                </li>
+
+                <li class="top06__contact">
+                    <div class="threecol threecol--1">
+                        <i class="fa fa-envelope-o icon"></i>
+                        <!-- <a href="mailto:bacforwork@gmail.com">bacforwork@gmail.com</a> -->
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=bacforwork@gmail.com">bacforwork@gmail.com</a>
+                    </div>
+                    <div class="threecol threecol--1">
+                        <i class="fa fa-tablet icon"></i>
+                        <a href="tel:01262567539">01262567539</a>
+                    </div>
+                    <div class="threecol threecol--1">
+                        <i class="fa fa-map-o icon"></i>
+                        <a href="https://goo.gl/maps/vQqPBsQigDE2" target="_blank">484 Le Van Viet, Dist. 9, Ho Chi Minh City</a>
+                    </div>
+                </li>
+            </ul>
+        </form>
+    </section>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+    <script src="js/luuhoangbac.js"></script>
+
+
+    <!-- <section class="top05">
+        <ul>
+                <p class="fourcol__content">I meet the requirement of speaking, listening, writing and reading. That helps me working well</p>
+                <p class="fourcol__content">I meet the requirement of speaking, listening, writing and reading. That helps me working well</p>
+                <p class="fourcol__content">I meet the requirement of speaking, listening, writing and reading. That helps me working well</p>
+                <p class="fourcol__content">I meet the requirement of speaking, listening, writing and reading. That helps me working well</p>
+                
+            <li class="top05__col1">
+
+            </li>
+        </ul>
+    </section> -->
+</body>
+
+</html>
